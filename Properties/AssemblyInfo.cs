34cec1910f1d21d2df52a3428ff856fb3b14ac0e
ebrahimelsayed23 <121295548+ebrahimelsayed23@users.noreply.github.com>
2022-12-23 15:14:14 +0200

    Add files via upload

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("diescrete task")]
+[assembly: AssemblyTitle("discrete task2")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("diescrete task")]
+[assembly: AssemblyProduct("discrete task2")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("1523bc66-ff61-46c1-9374-4f217464bc89")]
+[assembly: Guid("ff3b2437-7006-4d24-a350-27df4548c138")]
 
 // Version information for an assembly consists of the following four values:
 //
